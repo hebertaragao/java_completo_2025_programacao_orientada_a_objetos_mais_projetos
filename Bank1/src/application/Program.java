6db245f70f7b92e17a20b6befc6e5ef268e85882
hebertaragao <hebert.aragao@outlook.com>
2023-10-12 15:17:03 -0300

    Update Program.java, SavingAccount.java and BusinesAccount.java, implementation @Override

--- Bank1/src/application/Program.java
+++ Bank1/src/application/Program.java
@@ -36,6 +36,17 @@ public class Program {
 			acc5.updateBalance();
 			System.out.println("Update!");
 		}
+		Account acc6 = new Account(1005, "Rob", 1000.0);
+		acc6.withdraw(200.0);
+		System.out.println(acc6.getBalance());
+		
+		Account acc7 = new SavingsAccount(1006, "Ken", 1000.0, 0.01);
+		acc7.withdraw(200.0);
+		System.out.println(acc7.getBalance());
+		
+		Account acc8 = new BusinessAccount(1007, "Ryu", 1000.0, 500.0);
+		acc8.withdraw(200.0);
+		System.out.println(acc8.getBalance());
 	}
 
 }
